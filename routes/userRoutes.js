7a0d3369a67018f0d098a7b517571a4cee66ebed
Jared Avila <jared.c.avila@gmail.com>
2019-11-18 09:20:09 -0800

    working on user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const User = require("../models/userModel");
 
 const router = express.Router();
 
+const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: {
+        users
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      message: err
+    });
+  }
+};
+
 router.route("/login").post(juvicountController.login);
 router.route("/register").post(juvicountController.register);
+
+router.route("/").get(getAllUsers);
 
 module.exports = router;
