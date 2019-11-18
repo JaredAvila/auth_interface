af74a744cb897e50653c64dd6fc9db51352a878b
Jared Avila <jared.c.avila@gmail.com>
2019-11-18 12:54:08 -0800

    working on user routes - creating child account/verification

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/userModel");
+const AppError = require("../utils/AppError");
 
 const router = express.Router();
 
@@ -24,6 +27,7 @@ const getAllUsers = async (req, res, next) => {
 
 const register = async (req, res, next) => {
   try {
+    // create new user from form data
     const newUser = await User.create({
       name: req.body.name,
       email: req.body.email,
@@ -34,7 +38,12 @@ const register = async (req, res, next) => {
       photo: req.body.photo
     });
 
-    const token = null;
+    // create and sign token
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN
+    });
+
+    // send back token and user data
     res.status(200).json({
       status: "success",
       token,
@@ -42,6 +51,73 @@ const register = async (req, res, next) => {
         newUser
       }
     });
+
+    // catch errors and send 400 error
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      message: err
+    });
+  }
+};
+
+const registerChild = async (req, res, next) => {
+  try {
+    // 1) get token and check that it exists
+    let token = null;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      return next(
+        new AppError("You are not logged in. Please login to continue.", 401)
+      );
+    }
+
+    // 2) validate token
+    const decodedToken = await promisify(jwt.verify)(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    // 3) check if user still exists
+    const currentUser = await User.findById(decodedToken.id);
+    if(!currentUser){
+        return next(new AppError('This user no longer exists', 401));
+    }
+
+    // 4) check if user changed passwords since token was issued
+    
+
+    // create new child account from form data
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2,
+      passwordChangedAt: req.body.passwordChangedAt,
+      role: req.body.role,
+      photo: req.body.photo
+    });
+
+    // create and sign token
+    // const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    //   expiresIn: process.env.JWT_EXPIRES_IN
+    // });
+
+    // send back token and user data
+    res.status(200).json({
+      status: "success",
+      token,
+      data: {
+        newUser
+      }
+    });
+
+    // catch errors and send 400 error
   } catch (err) {
     res.status(400).json({
       status: "error",
