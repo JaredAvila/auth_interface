c2b532eb798d82bbf1711a02ad893f306d586579
Jared Avila <jared.c.avila@gmail.com>
2019-12-09 21:25:07 -0800

    adding security and sanitization

--- app.js
+++ app.js
@@ -9,16 +9,39 @@ const childRouter = require("./routes/childRoutes");
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// GLOBAL MIDDLEWARE
 
-app.use(express.json());
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in one hour"
+});
+app.use("/api", limiter);
+
+// Body parser, reading data into body
+app.use(express.json({ limit: "10kb" }));
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/item", itemRouter);
