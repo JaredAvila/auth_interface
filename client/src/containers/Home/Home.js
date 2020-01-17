e04451490621d9077c938876649eed88cf26a2dc
Jared Avila <jared.c.avila@gmail.com>
2020-01-16 16:38:00 -0800

    working on home component

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import { Container, Grid } from "@material-ui/core";
 
+import * as classes from "./Home.module.css";
+
 import { connect } from "react-redux";
 
 const Home = props => {
@@ -11,14 +13,17 @@ const Home = props => {
   console.log(props.user);
   return (
     <Container>
-      <div>
+      <div className={classes.Top}>
         <img
           src={`http://127.0.0.1:8000/img/users/${props.user.photo}`}
           alt={props.user.name}
         />
-      </div>
-      <div>
         <h1>{props.user.name}</h1>
+      </div>
+      <div className={classes.Bot}>
+        <button>Press me</button>
+        <button>Press me</button>
+        <button>Press me</button>
       </div>
     </Container>
   );
